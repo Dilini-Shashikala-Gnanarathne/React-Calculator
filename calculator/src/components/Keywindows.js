8cbdc9d1ff3426cb02f28823890b1ffb15962fa7
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-06-26 23:51:40 +0530

    change css

--- calculator/src/components/Keywindows.js
+++ calculator/src/components/Keywindows.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import '../App.css';
 
 const Keywindows = () => {
   const sciKey = ['cos', 'sin', 'ln', 'e', 'log', 'tan', 'pi', '^', '!', '√'];
   const basicKey = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '=', 'C', '.', '±', '%'];
 
   return (
-    <div>
-      <div>
+    <div className="keywindows-container">
+      <div className="scientific-keys">
         <h2>Scientific Keys</h2>
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <div className="keys">
           {sciKey.map(key => (
-            <button key={key} style={{ margin: '5px' }}>
+            <button key={key} className="key-button">
               {key}
             </button>
           ))}
         </div>
       </div>
-      <div>
+      <div className="basic-keys">
         <h2>Basic Keys</h2>
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <div className="keys">
           {basicKey.map(key => (
-            <button key={key} style={{ margin: '5px' }}>
+            <button key={key} className="key-button">
               {key}
             </button>
           ))}
